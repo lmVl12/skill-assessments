1b466bd90a64cdaab538b2f64853c94fd119d691
mila <lucy.mar4enko@gmail.com>
2026-05-28 11:16:13 +0300

    feat(basic-web-dev): add plotly and simple navbar

--- Basic Data Science Web Development/Gapminder/app.R
+++ Basic Data Science Web Development/Gapminder/app.R
@@ -1,50 +1,53 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(tidyverse) # Core suite for data manipulation and visualization
+library(plotly) # Dynamic and interactive web-based plots
+library(scales) # Tools for formatting axis labels and legends
+library(naniar) # Specialized tools for missing data analysis
+library(broom) # Converts statistical models into tidy data frames
+library(knitr) # General-purpose tool for dynamic report generation
+library(kableExtra) # Advanced styling for HTML and PDF tables
+library(viridis)
+library(shinyWidgets)
 
-# Define UI for application that draws a histogram
+gapminder_data <- read_csv("../gapminder_clean.csv", col_select = -1)
+available_years <- sort(unique(gapminder_data$Year))
+
+# Define UI for application 
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("Gapminder Analysis"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+          selectInput("x", "X variable", choices = names(gapminder_data), selected = "gdpPercap"),
+          selectInput("y", "Y variable", choices = names(gapminder_data), selected = "CO2 emissions (metric tons per capita)"),
+          selectInput("color", "Map to color", choices = names(gapminder_data), selected = "continent"),
+          sliderTextInput("year",
+                          "Year",
+                          grid = TRUE,
+                          choices = available_years,
+                          selected = min(available_years)),
+          checkboxInput("log_x", "Log scale X", FALSE),
+          checkboxInput("log_y", "Log scale Y", FALSE)
+          ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          plotlyOutput("plot")
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$plot <- renderPlotly({
+    plot_data <- gapminder_data |> 
+      filter(Year == as.numeric(input$year))
+    
+    p <- ggplot(plot_data, aes(.data[[input$x]], .data[[input$y]])) +
+      geom_point(aes(color = .data[[input$color]]))
+    if (input$log_x) p <- p + scale_x_log10()
+    if (input$log_y) p <- p + scale_y_log10()
+    ggplotly(p)
+  })
 }
 
 # Run the application 
